bcd11da5d44ee0e7caacc5386a3ca5159a255b36
Your Name <you@example.com>
2017-08-21 21:18:40 +0800

    chuzu

---
+++ js/js.js
@@ -0,0 +1,50 @@
+// JavaScript Document
+
+function over(){
+			document.getElementById('shequ').style.display = 'block';
+			document.getElementById('leibie').style.display = 'none';
+			document.getElementById('huxing').style.display = 'none';
+			document.getElementById('laiyuan').style.display = 'none';
+		}
+function out(){
+			document.getElementById('shequ').style.display = 'none';
+		}
+function over2(){
+			document.getElementById('leibie').style.display = 'block';
+			document.getElementById('shequ').style.display = 'none';
+			document.getElementById('huxing').style.display = 'none';
+			document.getElementById('laiyuan').style.display = 'none';
+		}
+function out2(){
+			document.getElementById('leibie').style.display = 'none';
+		}
+function over3(){
+			document.getElementById('huxing').style.display = 'block';
+			document.getElementById('shequ').style.display = 'none';
+			document.getElementById('leibie').style.display = 'none';
+			document.getElementById('laiyuan').style.display = 'none';
+		}
+function out3(){
+			document.getElementById('huxing').style.display = 'none';
+		}		
+function over4(){
+			document.getElementById('laiyuan').style.display = 'block';
+			document.getElementById('shequ').style.display = 'none';
+			document.getElementById('leibie').style.display = 'none';
+			document.getElementById('huxing').style.display = 'none';
+		}
+function out4(){
+			document.getElementById('laiyuan').style.display = 'none';
+		}		
+function over5(){
+			document.getElementById('shequ').style.display = 'none';
+			document.getElementById('leibie').style.display = 'none';
+			document.getElementById('huxing').style.display = 'none';
+			document.getElementById('laiyuan').style.display = 'none';
+	}	
+		
+		
+		
+		
+		
+		
